5793f016bf35da006fa21fbb95882ea0f3e661c1
Shapovalov Dmytro <buonviagio@gmail.com>
2023-03-10 11:50:37 +0200

    My first commit Module3 Final

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int first = (number % 10);
+        number /= 10;
+        int second = number % 10;
+        number /= 10;
+        int third = number % 10;
+        number /= 10;
+        number += first + second + third;
+        System.out.println(number);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        float tmp = 9f;
+        System.out.println((temperatureCelsius * (tmp/5)) + 32);
     }
 }
